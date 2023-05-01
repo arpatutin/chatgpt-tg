1477ca40d3c0803ce22429a237cca79e7fc69a37
Arsenii Patutin <arpatutin@outlook.com>
2023-05-01 20:47:12 +0400

    otsosite penis ya nauchilsa kommitit'

---
+++ locales/en.py
@@ -0,0 +1,8 @@
+en_locale = {
+    "HELLO": "Hello!\nThis bot is made for accessing ChatGPT from Russia. Please note, that we don't"
+             "support block preventing, and our goal is to satisfy your curiosity.",
+    "HELP": "Here's the list of available options:\n/startconvestation - create a new chat. A chat is an object that"
+             "saves your messages not to loose a context.\n/quitconvestation - leave chat. It'll be saved and"
+             "you'll be able to reopen it later\n/finishconvestation - leave and remove "
+             "the chat. In this case you won't be able to continue the conversation in your circumstances."
+}
---
+++ main.py
@@ -0,0 +1,63 @@
+from telebot.types import *
+import telebot
+
+
+from config import *
+from locales.ru import *
+from locales.en import *
+
+bot = telebot.TeleBot(TELEGRAM_KEY)
+locales = {}  # User ID: locale
+
+
+# db
+
+
+@bot.callback_query_handler(func=lambda _: True)
+def set_locale(call: CallbackQuery):
+    global locales
+    if call.data in ["set_ru_locale", "set_en_locale"]:
+        if call.data == "set_ru_locale":
+            locales[call.from_user.id] = ru_locale
+        elif call.data == "set_en_locale":
+            locales[call.from_user.id] = en_locale
+        bot.answer_callback_query(call.id, "OK")
+        bot.send_message(call.message.chat.id, locales[call.from_user.id]["HELLO"])
+        bot.send_message(call.message.chat.id, locales[call.from_user.id]["HELP"])
+
+
+@bot.message_handler(commands=["start"])
+def start(message: Message):
+    if locales.get(message.from_user.id) is None:
+        markup = InlineKeyboardMarkup()
+        markup.add(InlineKeyboardButton("Русский", callback_data="set_ru_locale"))
+        markup.add(InlineKeyboardButton("English", callback_data="set_en_locale"))
+        bot.send_message(message.chat.id, "Choose your language / Выберите свой язык", reply_markup=markup)
+
+
+@bot.message_handler(commands=["help"])
+def help_(message: Message):
+    bot.send_message(message.chat.id, locales[message.from_user.id]["HELP"])
+
+
+@bot.message_handler(commands=["startconversation"])
+def startconversation(message: Message):
+    pass
+
+
+@bot.message_handler(commands=["quitconversation"])
+def quitconversation(message: Message):
+    pass
+
+
+@bot.message_handler(commands=["rejoinconversation"])
+def rejoinconversation(message: Message):
+    pass
+
+
+@bot.message_handler(commands=["finishconversation"])
+def finishconversation(message):
+    pass
+
+
+bot.polling(non_stop=True)
