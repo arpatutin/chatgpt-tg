d80e63abbb53b9bade1607c95fb39b5123b654b7
Arsenii Patutin <arpatutin@outlook.com>
2023-05-04 22:02:36 +0400

    Sense correction

--- locales/en.py
+++ locales/en.py
@@ -1,6 +1,5 @@
 en_locale = {
-    "HELLO": "Hello!\nThis bot is made for accessing ChatGPT from Russia. Please note, that we don't "
-             "support block preventing, and our goal is to satisfy your curiosity.",
+    "HELLO": "Hello!\nThis bot is made for accessing ChatGPT from Telegram.",
     "HELP": "Here's the list of available options:\n/help - call this menu.\n/changelang - call the language change "
             "menu.\n/startconversation - create a new chat. A chat is an object that "
              "saves your messages not to loose a context.\n/quitconversation - leave chat. It'll be saved and "
